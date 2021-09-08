e7cbbd9607f803bb3cda90faa866c23c9aed6548
github username <cristi.nord@gmail.com>
2021-09-08 16:23:13 +0100

    Object Literals p1

--- test.ts
+++ test.ts
@@ -1,16 +1,48 @@
-let displayColor = function (message, ...colors) {
-  console.log(message);
-  //   console.log(colors);
-  console.log(arguments, arguments.length);
-  for (let i in colors) {
-    console.log(colors[i]);
-  }
+// let displayColor = function (message, ...colors) {
+//   console.log(message);
+//   //   console.log(colors);
+//   console.log(arguments, arguments.length);
+//   for (let i in colors) {
+//     console.log(colors[i]);
+//   }
+// };
+
+// let message = 'List of Colors';
+// let colorArray = ['Orange', 'Yellow', 'Indigo'];
+// displayColor(message, ...colorArray);
+
+// // displayColor(message, 'Red');
+// // displayColor(message, 'Red', 'Blue');
+// // displayColor(message, 'Red', 'Blue', 'Green');
+
+// Object Literals p1
+
+let firstName = 'Chandler';
+let lastName = 'Bings';
+
+let person = {
+  firstName,
+  lastName,
 };
 
-let message = 'List of Colors';
-let colorArray = ['Orange', 'Yellow', 'Indigo'];
-displayColor(message, ...colorArray);
+function createPerson(firstName, lastName, age) {
+  let fullName = `${firstName} ${lastName}`;
+  return {
+    firstName,
+    lastName,
+    fullName,
+    isSenior() {
+      return age > 60;
+    },
+  };
+}
 
-// displayColor(message, 'Red');
-// displayColor(message, 'Red', 'Blue');
-// displayColor(message, 'Red', 'Blue', 'Green');
+let p = createPerson('Ross', 'Geller', 62);
+
+console.log(p);
+console.log(p.firstName);
+console.log(p.lastName);
+console.log(p.fullName);
+console.log(p.isSenior());
+// console.log(person.firstName);
+// console.log(person.lastName);
