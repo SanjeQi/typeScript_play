47e99c2b5b0d2a6846b659725841b161c48f8854
github username <cristi.nord@gmail.com>
2021-09-08 20:15:12 +0100

    Class Methods *constructor*static*prototype

--- scripts/test.js
+++ scripts/test.js
@@ -1,17 +1,20 @@
 "use strict";
-// Class
-// let p1 = new Person();
 class Person {
+    //using a constructor method - object creation - only one constructor per class definition
+    constructor(name) {
+        this.name = name;
+        console.log(this.name + ' Constructor');
+    }
+    // using a static method ---called with Person.staticMethod(), so can be called with the class and not the object
+    static staticMethod() {
+        console.log('Static Method');
+    }
+    // using a prototype method
+    greet() {
+        console.log(`Hello ${this.name}`);
+    }
 }
-let p = new Person();
-//classes are functions but ....Functions are hoisted, classes are not!!!
-function employee() { }
-employee();
-console.log(typeof Person);
-class Animal {
-    greet() { }
-}
-let a = new Animal();
-console.log(Animal.prototype);
-console.log(a.greet === Animal.prototype.greet);
+let p = new Person('Chandler');
+Person.staticMethod();
+p.greet();
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,24 +1,21 @@
-// Class
-
-// let p1 = new Person();
-
-class Person {}
-let p = new Person();
-
-//classes are functions but ....Functions are hoisted, classes are not!!!
-
-function employee() {}
-employee();
-
-console.log(typeof Person);
-
-class Animal {
-  greet() {}
+class Person {
+  //using a constructor method - object creation - only one constructor per class definition
+  constructor(name) {
+    this.name = name;
+    console.log(this.name + ' Constructor');
+  }
+  // using a static method ---called with Person.staticMethod(), so can be called with the class and not the object
+  static staticMethod() {
+    console.log('Static Method');
+  }
+  // using a prototype method - can be called with each object ex : p :  p.greet()
+  greet() {
+    console.log(`Hello ${this.name}`);
+  }
 }
 
-let a = new Animal();
+let p = new Person('Chandler');
 
-console.log(Animal.prototype);
-console.log(a.greet === Animal.prototype.greet);
+Person.staticMethod();
 
-//adding a method to a class is like adding a method to the prototype. => Classes are jus syntactic sugar over prototype inheritance
+p.greet();
