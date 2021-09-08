0f3fdf0ee648d49ad795ee81f6348593c7e99086
github username <cristi.nord@gmail.com>
2021-09-08 15:47:37 +0100

    Rest and Spread Operator

---
+++ scripts/test.js
@@ -0,0 +1,16 @@
+"use strict";
+let displayColor = function (message, ...colors) {
+    console.log(message);
+    //   console.log(colors);
+    console.log(arguments, arguments.length);
+    for (let i in colors) {
+        console.log(colors[i]);
+    }
+};
+let message = 'List of Colors';
+let colorArray = ['Orange', 'Yellow', 'Indigo'];
+displayColor(message, ...colorArray);
+// displayColor(message, 'Red');
+// displayColor(message, 'Red', 'Blue');
+// displayColor(message, 'Red', 'Blue', 'Green');
+//# sourceMappingURL=test.js.map
