70a39ea4ebb4d155a50af89ddd03060122676911
github username <cristi.nord@gmail.com>
2021-09-08 17:09:21 +0100

    Destructuring - Arrays

--- scripts/test.js
+++ scripts/test.js
@@ -33,17 +33,38 @@
 // console.log(p.isSenior());
 // console.log(person.firstName);
 // console.log(person.lastName);
-// Object Literals p2
-// 1. Spaces in object properties
-const per = {
-    'first name': 'Chandler',
-};
-console.log(per['first name']);
-//2. Variables as properties name
-const ln = 'last name';
-const person = {
-    'first name': 'Chandler',
-    [ln]: 'Bing',
-};
-console.log(person);
+// // Object Literals p2
+// // 1. Spaces in object properties
+// const per = {
+//   'first name': 'Chandler',
+// };
+// console.log(per['first name']);
+// // 2. Variables as properties name
+// const ln = 'last name';
+// const person = {
+//   'first name': 'Chandler',
+//   [ln]: 'Bing',
+// };
+// console.log(person);
+//Destructuring - Arrays
+// Mapping one to one
+// let employee = ['Chandler', 'Bing', 'Male'];
+// let [firstName, lastName, gender] = employee;
+// console.log(firstName, lastName, gender);
+// 2 to 3 export
+// let employee = ['Chandler', 'Bing'];
+// let [firstName, lastName, gender] = employee;
+// console.log(firstName, lastName, gender);
+// omitting arguments
+// let employee = ['Chandler', 'Bing', 'Male'];
+// let [, , gender] = employee;
+// console.log(gender);
+// // with rest operator
+// let employee = ['Chandler', 'Bing', 'Male'];
+// let [firstName, ...elements] = employee;
+// console.log(firstName, elements);
+// // with default values
+let employee = ['Chandler', 'Bing'];
+let [firstName, lastName, gender = 'Male'] = employee;
+console.log(firstName, lastName, gender);
 //# sourceMappingURL=test.js.map
