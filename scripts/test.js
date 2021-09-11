08c9548207b692be097e33a4c79f9a1e9240784f
github username <cristi.nord@gmail.com>
2021-09-11 21:36:41 +0100

    WeakSet

--- scripts/test.js
+++ scripts/test.js
@@ -1,26 +1,18 @@
 "use strict";
-//Sets - Object - can only contain unique values
-let mySet = new Set();
-let ob1 = {};
-let ob2 = {};
-mySet.add('Hello');
-mySet.add(1);
-mySet.add(1); // not added to the set - not unique
-//Passing an object to the set
-mySet.add(ob1);
-mySet.add(ob2); //both objects are not converted to stings so that is why they are unique and are added to the set
-console.log(mySet, mySet.size);
-//Passing an Array to the set
-let newSet = new Set([1, 2, 3, 4, 4, 4]);
-console.log(newSet); // output: Set(4) {1, 2, 3, 4} -- only the unique values added
-//Chaining
-let chainSet = new Set().add('Hello').add('world');
-console.log(chainSet);
-//To check the existence of a value in a Set use .has() :ex: randomSetObject.has()
-console.log(newSet.has(5));
-//To delete a value from a set
-console.log(newSet.delete(4));
-console.log(newSet);
-console.log(newSet.delete(3));
-console.log(newSet);
+//Sets - Weak Sets
+//Similar to stong Sets but can store only store object references and not primitive values
+// let mySet = new Set();
+// let key = {};
+// mySet.add(key);
+// console.log('mySet', mySet);
+// console.log(mySet.size);
+// key = null;
+// console.log(mySet.size);
+// key = [...mySet][0];
+// console.log(key);
+//Create a week Set
+let set = new WeakSet();
+let key = {};
+set.add(key);
+console.log(set.has(key));
 //# sourceMappingURL=test.js.map
