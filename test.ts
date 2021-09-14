e82afd09ababa397b03effddf2223485c2c352d0
github username <cristi.nord@gmail.com>
2021-09-14 11:46:43 +0100

    forEach with Maps and Sets

--- test.ts
+++ test.ts
@@ -1,23 +1,29 @@
-// Maps - iterating over maps
+//forEach with Maps and Sets
 
+//forEach with arrays
+const numbers = [2, 4, 6, 8];
+numbers.forEach(arrayFunction);
+//arrayFunction definition
+function arrayFunction(element, index, array) {
+  console.log('arr[' + index + ']=' + element);
+}
+
+//forEach with Maps
 let myMap = new Map([
   ['fname', 'Chandler'],
   ['lname', 'Bing'],
 ]);
-
-// Getting the keys - fname, lname
-for (let key of myMap.keys()) {
-  console.log(key);
+myMap.forEach(mapFunction);
+//mapFunction definition
+function mapFunction(value, key, callingMap) {
+  console.log('key:', key, 'value:', value);
+  console.log(myMap === callingMap);
 }
-// Getting the values - Chandler , Bing
-for (let value of myMap.values()) {
-  console.log(value);
-}
-//Getting the keys and the values
-for (let entry of myMap.entries()) {
-  console.log('entries in myMap', entry);
-}
-//Getting the keys and values using destructuring
-for (let [key, value] of myMap.entries()) {
-  console.log(key, value);
+//forEach with Sets
+let mySet = new Set([1, 2, 3]);
+mySet.forEach(setFunction);
+//setFunction definition
+function setFunction(value, key, callingSet) {
+  console.log('key:', key, 'value:', value);
+  console.log(mySet === callingSet);
 }
