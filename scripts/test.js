6047442fe824d1d69936396c473d9dd440346b36
github username <cristi.nord@gmail.com>
2021-09-16 16:47:31 +0100

    TypeScript Enum*Arrays*Tuples* ClassConstructor*Interfaces

--- scripts/test.js
+++ scripts/test.js
@@ -1,13 +1,46 @@
 "use strict";
 //TypeScript
-//Declarations and Annotations
-let x = 10;
-let y = 'hi';
-let z = true;
-// this is done by default:
-let a = 100;
-//a = true; //<-- errors out boolean is not assignable to type number
-let b = a + 2; // ok
-b = a + 'hi';
-console.log(b);
+//Enumeration - enum type - used to create a custom type where we can specify a list of values that a variable can hold
+//in other words - a user friendly value for a particular numeric value
+var EyeColor;
+(function (EyeColor) {
+    EyeColor[EyeColor["Brown"] = 1] = "Brown";
+    EyeColor[EyeColor["Black"] = 2] = "Black";
+    EyeColor[EyeColor["Blue"] = 3] = "Blue";
+    EyeColor[EyeColor["Green"] = 4] = "Green";
+})(EyeColor || (EyeColor = {}));
+let myEyeColor = EyeColor.Black; //"black" is the name for the value 2 in this case
+console.log(myEyeColor); // output: 2
+console.log(EyeColor[myEyeColor]); //output:Black
+//Arrays
+let strArr1 = ['Chandler', 'Bing']; //method1
+let strArr2 = ['Chandler', 'Bing']; //method2
+let anyArr = ['Chandler', 100]; //method3
+//Tuple
+//*are special Arrays that let us specify the type of values, the array can contain
+let myTuples = ['Chandler', 100];
+console.log(myTuples);
+myTuples.push('Bing', 200);
+console.log(myTuples);
+//Class Constructor -- v2 is equivalent to v1
+//v1
+class Person {
+    constructor(fname, lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+}
+//v2
+class Person1 {
+    constructor(fname, lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+}
+let parsnip1 = {
+    fname: 'Chandler',
+    lname: 'Bing',
+    age: 30,
+};
+console.log(parsnip1);
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,19 +1,61 @@
 //TypeScript
 
-//Declarations and Annotations
-let x: number = 10;
-let y: string = 'hi';
-let z: boolean = true;
-//----------------------------------------------
-// this is done by default:
-let a = 100;
-//a = true; //<-- errors out boolean is not assignable to type number
-let b: number = a + 2; // ok
-b = a + 'hi'; //
-console.log(b);
-//----------------------------------------------
-let info: any;
-info = 10;
-info = 'hello';
-info = true;
-//----------------------------------------------
+//Enumeration - enum type - used to create a custom type where we can specify a list of values that a variable can hold
+//in other words - a user friendly value for a particular numeric value
+enum EyeColor {
+  Brown = 1,
+  Black,
+  Blue,
+  Green,
+}
+
+let myEyeColor = EyeColor.Black; //"black" is the name for the value 2 in this case
+console.log(myEyeColor); // output: 2
+console.log(EyeColor[myEyeColor]); //output:Black
+//Arrays
+let strArr1: string[] = ['Chandler', 'Bing']; //method1
+let strArr2: Array<string> = ['Chandler', 'Bing']; //method2
+let anyArr: any[] = ['Chandler', 100]; //method3
+
+//Tuple
+//*are special Arrays that let us specify the type of values, the array can contain
+
+let myTuples: [string, number] = ['Chandler', 100];
+console.log(myTuples);
+myTuples.push('Bing', 200);
+console.log(myTuples);
+
+//Class Constructor -- v2 is equivalent to v1
+//v1
+class Person {
+  public fname: string;
+  public lname: string;
+  constructor(fname: string, lname: string) {
+    this.fname = fname;
+    this.lname = lname;
+  }
+}
+//v2
+class Person1 {
+  constructor(public fname: string, public lname: string) {}
+}
+
+//Interfaces - the most flexible way of describing types in TypeScript
+
+interface Parsnip {
+  fname: string;
+  lname: string;
+  age?: number; //age?: will make the property optional
+}
+
+let parsnip1: Parsnip = {
+  fname: 'Chandler',
+  lname: 'Bing',
+  age: 30,
+};
+
+console.log(parsnip1);
+let parsnip2: Parsnip = {
+  fname: 'Cosmo',
+  lname: 'Kramer',
+};
