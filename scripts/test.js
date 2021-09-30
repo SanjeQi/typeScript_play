8c43d60b975051c9efbc044aff531f65e3f19de5
github username <cristi.nord@gmail.com>
2021-09-30 18:29:37 +0100

    //Generic Function Currying - solution 2

--- scripts/test.js
+++ scripts/test.js
@@ -1,11 +1,10 @@
 "use strict";
-//Function Currying Problem - solution 1
+//Generic Function Currying - solution 1
 //Implement a generic curry function
 //Write a function that takes any function and returns a curried version of the function
-function sum(a, b) {
-    return a + b;
+function sum(a, b, c, d) {
+    return a + b + c + d;
 }
-sum(300, 10);
 // let curriedSum = genericCurry(sum);
 // curriedSum(2)(3);
 // curriedSum(2, 3);
@@ -22,7 +21,22 @@ function genericCurryOne(f) {
         }
     };
 }
-let curriedSum = genericCurryOne(sum);
-console.log(curriedSum(2)(3));
-console.log(curriedSum(2, 9));
+let curriedSumOne = genericCurryOne(sum);
+console.log(curriedSumOne(1)(2)(3)(4));
+console.log(curriedSumOne(1, 2)(3)(4));
+console.log(curriedSumOne(1, 2, 3)(4));
+console.log(curriedSumOne(1, 2, 3, 4));
+//Solution 2
+function genericCurryTwo(f) {
+    return function currify(...args) {
+        return args.length >= f.length
+            ? f.apply(this, args)
+            : currify.bind(this, ...args);
+    };
+}
+let curriedSumTwo = genericCurryTwo(sum);
+console.log(curriedSumTwo(1)(2)(3)(4));
+console.log(curriedSumTwo(1, 2)(3)(4));
+console.log(curriedSumTwo(1, 2, 3)(4));
+console.log(curriedSumTwo(1, 2, 3, 4));
 //# sourceMappingURL=test.js.map
