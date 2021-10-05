793ea5effda8e81b3587944f22dbce718df29fc7
github username <cristi.nord@gmail.com>
2021-10-05 17:16:50 +0100

    Class keyword

--- scripts/test.js
+++ scripts/test.js
@@ -1,75 +1,53 @@
 "use strict";
-//Prototypal Inheritance
+//Class keyword
 /*
- Create a Programmer inheriting Person
-
-1.Add properties pertaining to the Programmer
-2.Add methods pertaining to the Programmer
-3.Inherit properties from Person
-4.Inherit methods from Person
-
-1.Add properties pertaining to the Programmer
-    function Programmer (){
-      this.language = language,
-    }
-
-2.Add methods pertaining to the Programmer
-    Programmer.prototype.code = function (){
-      console.log(`I'm coding in ${this.language}`);
-      
-    }
-3.Inherit properties from Person
-3.1 Programmer function will be invoked with the 'new' keyword
-    like this: const kramer = new Programmer('kramer','JavaScript')
-3.2 This object is created and return automatically
-
-    function Programer(name,language) {
-      // this = Object.create(Programer.prototype)
-      this.language = language
-      //return this
-    }
-
-3.3 Explicit bind 'this' in Programmer to Person() call
-    function Programmer (name, language){
-      //this = Object.create(Programer.prototype)
-      Person.call(this,name)
-      this.language = language
-      // return this
-    }
-4.Inherit methods from Person Link Programmer to Person.prototype Using Object.create()
-    function Programmer(name, language){
-      Person.call(this,name)
-      this.language=language
-    }
-Programer.prototype = Object.create(Person.prototype) */
-function Person(name) {
-    this.name = name;
+function Person1(name) {
+  this.name = name;
 }
-Person.prototype.sayMyname = function () {
-    console.log(`My name is ${this.name}`);
+
+Person1.prototype.sayMyName = function () {
+  console.log(`My name is ${this.name}`);
 };
-Person.prototype.eat = function (food) {
-    console.log(`I'm eating ${food}`);
+Person1.prototype.eat = function (food) {
+  console.log(`I'm eating ${food}`);
 };
-Person.prototype.sleep = function () {
-    console.log(`Sleeping ...`);
+Person1.prototype.sleep = function () {
+  console.log('Sleeping...');
 };
+
+const batman1 = new Person1('Bruce Wayne');
+const superman1 = new Person1('Clark Kent');
+
+console.log('Batman Object: ', batman1, 'Superman Object: ', superman1); */
+// Using class - Person class
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    sayMyName() {
+        console.log(`My name is ${this.name}`);
+    }
+    eat(food) {
+        console.log(`I'm eating ${food}`);
+    }
+    sleep() {
+        console.log('Sleeping');
+    }
+}
 const batman = new Person('Bruce Wayne');
 const superman = new Person('Clark Kent');
-// console.log('Batman Object: ', batman, 'Superman Object: ', superman);
-function Programmer(name, language) {
+console.log('Batman Object: ', batman, 'Superman Object: ', superman);
+/**
+ * Programer constructor Function
+ */
+function Programer(name, language) {
     Person.call(this, name);
     this.language = language;
 }
-Programmer.prototype = Object.create(Person.prototype);
-Programmer.prototype.code = function () {
+Programer.prototype = Object.create(Person.prototype);
+Programer.prototype.code = function () {
     console.log(`I'm coding in ${this.language}`);
 };
-const kramer = new Programmer('Superman', 'JavaScript');
-kramer.sayMyname();
-console.log(kramer.constructor); // Person --- it need to be Programmer
-Programmer.prototype.constructor = Programmer;
-console.log(kramer.constructor); // Programmer
-kramer instanceof Programmer; // true
-console.log('Kramer the coder ', kramer);
+Programer.prototype.constructor = Programer;
+console.log(Programer.prototype.constructor);
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,82 +1,72 @@
-//Prototypal Inheritance
+//Class keyword
 
+// Classes in JS are just syntactical sugar over prototypal inheritance
 /*
- Create a Programmer inheriting Person
-
-1.Add properties pertaining to the Programmer
-2.Add methods pertaining to the Programmer
-3.Inherit properties from Person
-4.Inherit methods from Person
-
-1.Add properties pertaining to the Programmer 
-    function Programmer (){
-      this.language = language,
-    }
-
-2.Add methods pertaining to the Programmer
-    Programmer.prototype.code = function (){
-      console.log(`I'm coding in ${this.language}`);
-      
-    }
-3.Inherit properties from Person
-3.1 Programmer function will be invoked with the 'new' keyword
-    like this: const kramer = new Programmer('kramer','JavaScript') 
-3.2 This object is created and return automatically
-
-    function Programer(name,language) {
-      // this = Object.create(Programer.prototype)
-      this.language = language 
-      //return this
-    }
-
-3.3 Explicit bind 'this' in Programmer to Person() call
-    function Programmer (name, language){
-      //this = Object.create(Programer.prototype)
-      Person.call(this,name)
-      this.language = language 
-      // return this
-    }
-4.Inherit methods from Person Link Programmer to Person.prototype Using Object.create()
-    function Programmer(name, language){
-      Person.call(this,name)
-      this.language=language
-    }
-Programer.prototype = Object.create(Person.prototype) */
-
-function Person(name) {
+function Person1(name) {
   this.name = name;
 }
 
-Person.prototype.sayMyname = function () {
+Person1.prototype.sayMyName = function () {
   console.log(`My name is ${this.name}`);
 };
-Person.prototype.eat = function (food) {
+Person1.prototype.eat = function (food) {
   console.log(`I'm eating ${food}`);
 };
-Person.prototype.sleep = function () {
-  console.log(`Sleeping ...`);
+Person1.prototype.sleep = function () {
+  console.log('Sleeping...');
 };
+
+const batman1 = new Person1('Bruce Wayne');
+const superman1 = new Person1('Clark Kent');
+
+console.log('Batman Object: ', batman1, 'Superman Object: ', superman1); */
+
+// Using class - Person class
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  sayMyName() {
+    console.log(`My name is ${this.name}`);
+  }
+  eat(food) {
+    console.log(`I'm eating ${food}`);
+  }
+  sleep() {
+    console.log('Sleeping');
+  }
+}
 
 const batman = new Person('Bruce Wayne');
 const superman = new Person('Clark Kent');
-// console.log('Batman Object: ', batman, 'Superman Object: ', superman);
 
-function Programmer(name, language) {
-  Person.call(this, name);
-  this.language = language;
-}
-Programmer.prototype = Object.create(Person.prototype);
-Programmer.prototype.code = function () {
+console.log('Batman Object: ', batman, 'Superman Object: ', superman);
+
+/**
+ * Programer constructor Function
+ * 
+  function Programer(name, language) {
+    Person.call(this, name);
+    this.language = language;
+  }
+
+Programer.prototype = Object.create(Person.prototype);
+Programer.prototype.code = function () {
   console.log(`I'm coding in ${this.language}`);
 };
+Programer.prototype.constructor = Programer;
 
-const kramer = new Programmer('Superman', 'JavaScript');
-kramer.sayMyname();
-console.log(kramer.constructor); // Person --- it need to be Programmer
+ */
 
-Programmer.prototype.constructor = Programmer;
+// Programer Class
 
-console.log(kramer.constructor); // Programmer
-kramer instanceof Programmer; // true
-
-console.log('Kramer the coder ', kramer);
+class Programer extends Person {
+  constructor(name, language) {
+    super(name); // this is calling the Person class constructor
+    this.language = language;
+  }
+  code() {
+    console.log(`I'm coding in ${this.language}`);
+  }
+}
