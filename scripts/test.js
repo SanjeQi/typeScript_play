76e256398e0dda6cdcc152213c19fba46f7230bb
SanjeQi <cristi.nord@gmail.com>
2021-10-29 17:11:22 +0100

    Make an object iterable

--- scripts/test.js
+++ scripts/test.js
@@ -1,50 +1,58 @@
 "use strict";
 // Iterables & Iterators
-// --- Implementing iterable protocol  ---
+// --- Make an object iterable  ---
+// use a for of loop to print numbers from 0 to 5o
 /*
- *  Iterable Protocol - the object should implement a method at the key [Symbol.iterator] and the method accepts no argument and returns an iterator
- */
-//1.Crate an empty object
-let obj = {};
-//2.  Implement iterable protocol
-//2.1 Implement a method at the key [Symbol.iterator]
-obj = {
-    [Symbol.iterator]: function () { },
+
+const range = {};
+for (let num of range) {
+  console.log(num);
+}
+
+*/
+//1. For an object to be an iterable, it must implement a method at the key [Symbol.Iterators]
+/*
+const range = {
+  [Symbol.iterator]: function () {},
 };
-//2.2 The method at 2.1 should not accept any argument and should return an object which conforms to the iterator protocol
-obj = {
+*/
+//2. [Symbol.iterator]() should not take any arguments and it should conform to the iterator protocol
+// const range = {
+//   [Symbol.iterator]: function () {
+//     const iterator = {};
+//     return iterator;
+//   },
+// };
+//3. An iterator is an object that has a method at the key next. That method should return an object which contains two properties: value and done
+// const range = {
+//   [Symbol.iterator]: function(){
+//     const iterator = {
+//       next:function(){
+//         {value:, done:}
+//       }
+//     };
+//     return iterator
+//   }
+// }
+//Solution
+const range = {
     [Symbol.iterator]: function () {
-        const iterator = {};
-        return iterator;
-    },
-};
-// --- Implementing iterator protocol  ---
-/**
- * ------ Iterator
- * is an object that has a method at the key 'next'
- * this method should return an object which contains two properties.'value' and 'done'
- */
-obj = {
-    [Symbol.iterator]: function () {
-        let step = 0;
+        let counter = 0;
         const iterator = {
             next: function () {
-                step++;
-                if (step === 1) {
-                    return { value: 'Hello', done: false };
+                const result = { value: counter, done: false };
+                if (counter <= 500) {
+                    counter++;
+                    return result;
                 }
-                else if (step === 2) {
-                    return { value: 'World', done: false };
-                }
-                else
-                    return { value: undefined, done: true };
+                return { value: undefined, done: true };
             },
         };
         return iterator;
     },
 };
-// What is happening now whe I run the for of loop on the obj
-for (const word of obj) {
-    console.log(word);
+for (let num of range) {
+    console.log(num);
 }
+console.log([...range]);
 //# sourceMappingURL=test.js.map
