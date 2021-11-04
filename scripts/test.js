69da8305003a896f82da498203dac7d9a733bca0
SanjeQi <cristi.nord@gmail.com>
2021-11-04 18:01:25 +0000

    Asynchronous JavaScript - Timeouts and Intervals

--- scripts/test.js
+++ scripts/test.js
@@ -1,24 +1,48 @@
 "use strict";
-// Create an iterable that will accept the starting number, the ending number and an interval. Generate a range out of it
-function* customRange(start = 0, end = 100, interval = 1) {
-    for (let i = start; i <= end; i += interval) {
-        yield i;
-    }
+// Asynchronous JavaScript - Timeouts and Intervals
+// Timeouts and Intervals
+/** ********** ****** setTimeout()  **********
+      * executes a particular block of code once, after a specified time has elapsed
+      
+setTimeout(fn, duration, param1, param2,...);
+        
+      -fn - first parameter is a function to run, or a reference to a function define elsewhere;
+      -duration - second parameter is a number representing the duration in milliseconds to wait before executing the   code;
+      -param1, param2 etc - after the second parameter: duration, I can pass any number of arguments that I want to pass to the function when it runs;
+
+      */
+//V1
+function greet1() {
+    console.log('Hello again');
 }
-const customGenerator = customRange(0, 100, 10);
-for (let num of customGenerator) {
-    console.log(num);
+setTimeout(greet1, 500); // "Hello again" will be logged to the console after 5s
+//V2
+function greet2(name) {
+    console.log(`Hello again ${name}`);
 }
-// the function at [Symbol.iterator] can also be a generator function
-const obj = {
-    [Symbol.iterator]: function* (start = 0, end = 50, interval = 1) {
-        for (let i = start; i <= end; i += interval) {
-            yield i;
-        }
-    },
-};
-for (let num of obj) {
-    console.log(num);
+setTimeout(greet2, 200, 'Bruce Wayne'); //=> Hello again Bruce Wayne
+/* ************ clearTimeout()  ************
+      - To clear a timeout, I can use the clearTimeout() method passing in the identifier returned by setTimeout
+      - Practical application is clearing the timeouts when the component is unmounting to free up resources and also to prevent code from executing incorrectly on an unmounted component
+*/
+function greet3() {
+    console.log('Ola');
 }
-console.log([...obj[Symbol.iterator](100, 500, 100)]);
+const timeoutId = setTimeout(greet3, 800);
+console.log(timeoutId); //=> 3
+// clearing the timeout with the id 3
+clearTimeout(timeoutId);
+/** ********** ****** setInterval()  **********
+      * executes the code over and over again at regular intervals
+      
+setInterval(fn, duration, param1, param2,...);
+        
+      -fn -first param is the code to execute
+      -duration - the duration in milliseconds until the next execution of the code
+      -param1, param2 etc - after the second parameter: duration, I can pass any number of arguments that I want to pass to the function when it runs;
+      -intervals keep running a task forever so they must be cleared when not needed anymore.
+
+      */
+const intervalId = setInterval(greet2, 200, 'Clarke Kent');
+clearInterval(intervalId);
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,26 +1,67 @@
-// Create an iterable that will accept the starting number, the ending number and an interval. Generate a range out of it
+// Asynchronous JavaScript - Timeouts and Intervals
 
-function* customRange(start = 0, end = 100, interval = 1) {
-  for (let i = start; i <= end; i += interval) {
-    yield i;
-  }
+// Timeouts and Intervals
+
+/* 
+1. Timers and intervals are not part of JavaScript itself. They are implemented by the browser and setTimeout and    setInterval are basically names given to that functionality in JS 
+2. The duration passed to setInterval() and setTimeout() is the minimum delay, not the guaranteed delay.
+3. It is possible to achieve the dame effect as setInterval with a recursive setTimeout()
+      3.1 Duration is guaranteed between executions:
+      setTimeout(function run(){
+        console.log('some text')
+        setTimeout(run,100)
+      },100)
+      3.2 The duration interval includes the time taken to execute the code I want to run:
+      setInterval(function run(){
+        console.log('some other text');      
+      },100)
+
+*/
+
+/** ********** ****** setTimeout()  **********
+      * executes a particular block of code once, after a specified time has elapsed
+      
+setTimeout(fn, duration, param1, param2,...);
+        
+      -fn - first parameter is a function to run, or a reference to a function define elsewhere;
+      -duration - second parameter is a number representing the duration in milliseconds to wait before executing the   code;
+      -param1, param2 etc - after the second parameter: duration, I can pass any number of arguments that I want to pass to the function when it runs;
+
+      */
+//V1
+function greet1() {
+  console.log('Hello again');
 }
+setTimeout(greet1, 500); // "Hello again" will be logged to the console after 5s
 
-const customGenerator = customRange(0, 100, 10);
-
-for (let num of customGenerator) {
-  console.log(num);
+//V2
+function greet2(name) {
+  console.log(`Hello again ${name}`);
 }
+setTimeout(greet2, 200, 'Bruce Wayne'); //=> Hello again Bruce Wayne
 
-// the function at [Symbol.iterator] can also be a generator function
-const obj = {
-  [Symbol.iterator]: function* (start = 0, end = 50, interval = 1) {
-    for (let i = start; i <= end; i += interval) {
-      yield i;
-    }
-  },
-};
-for (let num of obj) {
-  console.log(num);
+/* ************ clearTimeout()  ************
+      - To clear a timeout, I can use the clearTimeout() method passing in the identifier returned by setTimeout
+      - Practical application is clearing the timeouts when the component is unmounting to free up resources and also to prevent code from executing incorrectly on an unmounted component
+*/
+function greet3() {
+  console.log('Ola');
 }
-console.log([...obj[Symbol.iterator](100, 500, 100)]);
+const timeoutId = setTimeout(greet3, 800);
+console.log(timeoutId); //=> 3
+// clearing the timeout with the id 3
+clearTimeout(timeoutId);
+
+/** ********** ****** setInterval()  **********
+      * executes the code over and over again at regular intervals
+      
+setInterval(fn, duration, param1, param2,...);
+        
+      -fn -first param is the code to execute
+      -duration - the duration in milliseconds until the next execution of the code
+      -param1, param2 etc - after the second parameter: duration, I can pass any number of arguments that I want to pass to the function when it runs;
+      -intervals keep running a task forever so they must be cleared when not needed anymore.
+
+      */
+const intervalId = setInterval(greet2, 200, 'Clarke Kent');
+clearInterval(intervalId);
