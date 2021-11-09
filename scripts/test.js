075c12df0344f31762b952cca46fcfd8c022756a
SanjeQi <cristi.nord@gmail.com>
2021-11-09 12:45:44 +0000

    Promise -part2

--- scripts/test.js
+++ scripts/test.js
@@ -1,32 +1,102 @@
 "use strict";
+// /** Promise
+//  *What?
+//     * A promise is simply an object
+//     A promise is always in one of the three states:
+//           * pending:  which is the initial state of a promise, neither fulfilled or rejected
+//           * fulfilled: the operation completed successfully
+//           * rejected: the operation failed
+//  *Why?
+//     * Promises can help deal with the asynchronous code in a far more simpler way compared to callbacks
+//     * Callback hell can be avoided with promises
+//  *JS
+//     1.Promise
+//     2.Promise Value
+//     3.Fulfill promise
+//     4.Reject promise
+//     5.Success callback
+//     6.Failure calLback
+//  * */
+// // a.How to create a promise?
+// const promise1 = new Promise();
+// // b.How to fulfill or reject a promise
+// const promise2 = new Promise((resolve, reject) => {}); // resolve and reject are functions :
+// const promise3 = new Promise((resolve, reject) => {
+//   //change the status from 'pending' to 'fulfilled'
+//   resolve();
+// });
+// const promise4 = new Promise((resolve, reject) => {
+//   //change status from 'pending' to 'rejected'
+//   reject();
+// });
+// // c.HOw to execute a callback function based on whether the Promise is fulfill or rejected
+// // *******  Resolve scenario ----------------------------------------------------
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     //Change status from 'pending' to 'fulfill'
+//     resolve();
+//   }, 5000);
+// });
+// //*********  Reject scenario
+// const promise = new Promise((resolve, reject) => {
+//   //Change status from 'pending' to 'rejected'
+//   reject();
+// }, 5000);
+// promise.then(onFulfillment);
+// promise.catch(onRejection);
+// //********************* Success and Failure callbacks ----------------------------
+// const onFulfillment = (result) => {
+//   // some code to process the result
+//   console.log(result);
+// };
+// const onRejection = (error) => {
+//   // code for error handling
+//   console.log(error);
+// };
+// //******************** Chaining Promises  ---------------------------- */
+// promise.then(onFulfillment).catch(onRejection);
+// Both then() and catch() return promises, and they can be chained
+// ******************** Promise - Static methods ************************ ------------------
+// *********** Promise.all() -------------------------------------------------------------------------
+// Query multiple API's and perform some action but only after all the API's have finished loading
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'foo');
+});
+Promise.all([promise1, promise2, promise3]).then((values) => {
+    console.log(values);
+});
+//=> [3, 42, 'foo']
 /**
- * ********* Callbacks *******
- *
- * Callbacks are functions passed as arguments to other functions
- * They can be synchronous if they execute immediately or they can be asynchronous where they get executed after some time has passed,some event has occured or some data has been fetched
- * As more and more request had to be made based on the data obtained from the previous requests,developer started to encounter what is known as the callbacks hell
- * Callback hell makes the code difficult to reason about
- * An alternative and the recommend approach now is to use Promises
+ * Promise.all() method takes an iterable of promises as an input and returns a single Promise that resolves to an array of the results of the input Promises.
+  
+ * Returned Promise will resolve when all the input promises have been resolved, or if the input iterable contains no Promises
+
+* It rejects immediately if any of the input promises reject or the non-promises throw an error, and will reject with the first rejection/error
  */
-// Synchronous Callbacks - invoked immediately
-function greet(name) {
-    console.log(`Hello my name is ${name}`);
-}
-function higherOrderFunction(callback) {
-    const name = 'Bruce Wayne';
-    callback(name);
-}
-////A Higher Order Function is a function that accepts a callback as an argument or it returns another function
-higherOrderFunction(greet);
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-const sort = numbers.sort((a, b) => a - b);
-const map = numbers.map((n) => n * 2);
-const filter = numbers.filter((n) => n % 2 === 0);
-console.log(sort, map, filter);
-//Asynchronous Callbacks used to delay the execution of a function until a particular time or event has occurred(data fetching)
-setTimeout(greet, 2000, 'Clark Kent');
-function callback() {
-    document.getElementById('demo').innerHTML = 'Change of text holder';
-}
-document.getElementById('demo').addEventListener('click', callback); // it waits for click to run the callback()
+// *********** Promise.allSettled() -------------------------------------------------------------------------
+Promise.allSettled([promise1, promise2, promise3]).then((values) => {
+    console.log(values);
+});
+/*  =>
+(3) [{…}, {…}, {…}]
+0: {status: 'fulfilled', value: 3}
+1: {status: 'fulfilled', value: 42}
+2: {status: 'fulfilled', value: 'foo'}
+length: 3
+[[Prototype]]: Array(0)
+*/
+//Promise.allSettled() waits for all input promises to complete regardless of whether or not one of them is rejected
+// *********** Promise.race() -------------------------------------------------------------------------
+// Returns promise that fulfills or rejects as soon as one of the input promises fulfills or rejects, with the value or reason from that promise
+const promise4 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 500, 'four');
+});
+const promise5 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'five');
+});
+Promise.race([promise4, promise5]).then((value) => {
+    console.log(value);
+});
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,63 +1,125 @@
-/** Promise
- *What?
-    * A promise is simply an object
-    
-    A promise is always in one of the three states:
-          * pending:  which is the initial state of a promise, neither fulfilled or rejected
-          * fulfilled: the operation completed successfully 
-          * rejected: the operation failed 
- *Why?
-    * Promises can help deal with the asynchronous code in a far more simpler way compared to callbacks
-    * Callback hell can be avoided with promises
- *JS
-    1.Promise
-    2.Promise Value
-    3.Fulfill promise
-    4.Reject promise
-    5.Success callback
-    6.Failure calLback
- * */
-// a.How to create a promise?
-const promise1 = new Promise();
-// b.How to fulfill or reject a promise
-const promise2 = new Promise((resolve, reject) => {}); // resolve and reject are functions :
+// /** Promise
+//  *What?
+//     * A promise is simply an object
 
+//     A promise is always in one of the three states:
+//           * pending:  which is the initial state of a promise, neither fulfilled or rejected
+//           * fulfilled: the operation completed successfully
+//           * rejected: the operation failed
+//  *Why?
+//     * Promises can help deal with the asynchronous code in a far more simpler way compared to callbacks
+//     * Callback hell can be avoided with promises
+//  *JS
+//     1.Promise
+//     2.Promise Value
+//     3.Fulfill promise
+//     4.Reject promise
+//     5.Success callback
+//     6.Failure calLback
+//  * */
+// // a.How to create a promise?
+// const promise1 = new Promise();
+// // b.How to fulfill or reject a promise
+// const promise2 = new Promise((resolve, reject) => {}); // resolve and reject are functions :
+
+// const promise3 = new Promise((resolve, reject) => {
+//   //change the status from 'pending' to 'fulfilled'
+//   resolve();
+// });
+
+// const promise4 = new Promise((resolve, reject) => {
+//   //change status from 'pending' to 'rejected'
+//   reject();
+// });
+
+// // c.HOw to execute a callback function based on whether the Promise is fulfill or rejected
+
+// // *******  Resolve scenario ----------------------------------------------------
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     //Change status from 'pending' to 'fulfill'
+//     resolve();
+//   }, 5000);
+// });
+
+// //*********  Reject scenario
+// const promise = new Promise((resolve, reject) => {
+//   //Change status from 'pending' to 'rejected'
+//   reject();
+// }, 5000);
+
+// promise.then(onFulfillment);
+// promise.catch(onRejection);
+
+// //********************* Success and Failure callbacks ----------------------------
+// const onFulfillment = (result) => {
+//   // some code to process the result
+//   console.log(result);
+// };
+
+// const onRejection = (error) => {
+//   // code for error handling
+//   console.log(error);
+// };
+// //******************** Chaining Promises  ---------------------------- */
+
+// promise.then(onFulfillment).catch(onRejection);
+
+// Both then() and catch() return promises, and they can be chained
+
+// ******************** Promise - Static methods ************************ ------------------
+
+// *********** Promise.all() -------------------------------------------------------------------------
+// Query multiple API's and perform some action but only after all the API's have finished loading
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
 const promise3 = new Promise((resolve, reject) => {
-  //change the status from 'pending' to 'fulfilled'
-  resolve();
+  setTimeout(resolve, 100, 'foo');
 });
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
+//=> [3, 42, 'foo']
+/**
+ * Promise.all() method takes an iterable of promises as an input and returns a single Promise that resolves to an array of the results of the input Promises.
+  
+ * Returned Promise will resolve when all the input promises have been resolved, or if the input iterable contains no Promises
+
+* It rejects immediately if any of the input promises reject or the non-promises throw an error, and will reject with the first rejection/error
+ */
+
+// *********** Promise.allSettled() -------------------------------------------------------------------------
+
+Promise.allSettled([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
+/*  =>
+(3) [{…}, {…}, {…}]
+0: {status: 'fulfilled', value: 3}
+1: {status: 'fulfilled', value: 42}
+2: {status: 'fulfilled', value: 'foo'}
+length: 3
+[[Prototype]]: Array(0)
+*/
+
+//Promise.allSettled() waits for all input promises to complete regardless of whether or not one of them is rejected
+
+// *********** Promise.race() -------------------------------------------------------------------------
+// Returns promise that fulfills or rejects as soon as one of the input promises fulfills or rejects, with the value or reason from that promise
 
 const promise4 = new Promise((resolve, reject) => {
-  //change status from 'pending' to 'rejected'
-  reject();
+  setTimeout(resolve, 500, 'four');
 });
 
-// c.HOw to execute a callback function based on whether the Promise is fulfill or rejected
-
-// *******  Resolve scenario ----------------------------------------------------
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    //Change status from 'pending' to 'fulfill'
-    resolve();
-  }, 5000);
+const promise5 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'five');
 });
 
-//*********  Reject scenario
-const promise = new Promise((resolve, reject) => {
-  //Change status from 'pending' to 'rejected'
-  reject();
-}, 5000);
+Promise.race([promise4, promise5]).then((value) => {
+  console.log(value);
+});
+// => five
 
-promise.then(onFulfillment);
-promise.catch(onRejection);
-
-//********************* Success and Failure callbacks ----------------------------
-const onFulfillment = (result) => {
-  // some code to process the result
-  console.log(result);
-};
-
-const onRejection = (error) => {
-  // code for error handling
-  console.log(error);
-};
+//both resolve but promise 2 is faster
