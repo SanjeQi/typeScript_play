d779cb01cb4ac31442d7a2c1b1f885c175713a7a
SanjeQi <cristi.nord@gmail.com>
2021-12-08 21:36:54 +0000

    Splice() and two arrays

--- test.ts
+++ test.ts
@@ -406,5 +406,30 @@ const longestWord = (str) => {
   }, '');
 };
 console.log(longestWord('My name is Superman'));
-console.log(longestWord('Cristi este numele meu'));
+console.log(longestWord('Batman has very expensive gear'));
 console.log(longestWord('m am   am a'));
+
+/* 
+--------------------------------------------------- Two arrays  -splice()  ------------------------------------------
+ 
+Given two arrays of integers (nums) and (index), create and return a new array (arr) which satisfies the following :
+
+From left to right read nums[i] and index[i] and insert at index index[i] the value nums[i] in array (arr) .
+
+Repeat the previous step until there are no elements to read in nums and index.
+
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+
+createArray(nums, index) - Returns [0,4,1,3,2]
+*/
+function createArray(nums, index) {
+  let target = [];
+  for (let i = 0; i < nums.length; i++) {
+    target.splice(index[i], 0, nums[i]);
+  }
+  return target;
+}
+
+const nums = [0, 1, 2, 3, 4];
+const index = [0, 1, 2, 2, 1];
+console.log(createArray(nums, index));
