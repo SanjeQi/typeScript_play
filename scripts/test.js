39c4ab35735e897dd0495fb870db6acc131ac89d
Sanje Qi <cristi.nord@gmail.com>
2022-12-07 16:24:39 +0000

    JS/TS replace empty strings in object with null

--- scripts/test.js
+++ scripts/test.js
@@ -137,4 +137,13 @@ const swapObjKeysWithValues = (obj) => {
 console.log(swapObjKeysWithValues({ z: 'a', y: 'b', x: 'c', w: 'd' })); //{a:'z',b:'y',c:'x',d:'w'}
 console.log(swapObjKeysWithValues({ 2: 'a', 4: 'b', 6: 'c', 8: 'd' })); //{a:'2',b:'4',c:'6',d:'8'}
 console.log(swapObjKeysWithValues({ a: 1, z: 24 })); //{1:'a',24:'z'}
+const replaceEmptyStrWithNull = (obj) => {
+    return Object.entries(obj).reduce((acc, [key, val]) => {
+        val.trim() === '' ? (val = null) : val;
+        return { ...acc, [key]: val };
+    }, {});
+};
+console.log(replaceEmptyStrWithNull({ a: 'a', b: 'b', c: '' })); //{ a: 'a', b: 'b', c: null }
+console.log(replaceEmptyStrWithNull({ a: '', b: 'b', c: ' ', d: 'd' }));
+console.log(replaceEmptyStrWithNull({ a: 'a', b: 'b ', c: ' ', d: '' }));
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -162,3 +162,18 @@ const swapObjKeysWithValues = (obj: ObjString) => {
 console.log(swapObjKeysWithValues({ z: 'a', y: 'b', x: 'c', w: 'd' })); //{a:'z',b:'y',c:'x',d:'w'}
 console.log(swapObjKeysWithValues({ 2: 'a', 4: 'b', 6: 'c', 8: 'd' })); //{a:'2',b:'4',c:'6',d:'8'}
 console.log(swapObjKeysWithValues({ a: 1, z: 24 })); //{1:'a',24:'z'}
+//===============================================Objects==========================================
+// Replace empty strings in object with null values
+// Write a function that takes an object as argument. Some of the property values contain empty strings. Replace empty strings and strings that contain only whitespace with null values. Return the resulting objec
+interface ObjStringNum {
+  [key: string | number]: string | number;
+}
+const replaceEmptyStrWithNull = (obj: ObjStringNum) => {
+  return Object.entries(obj).reduce((acc, [key, val]) => {
+    val.trim() === '' ? (val = null) : val;
+    return { ...acc, [key]: val };
+  }, {});
+};
+console.log(replaceEmptyStrWithNull({ a: 'a', b: 'b', c: '' })); //{ a: 'a', b: 'b', c: null }
+console.log(replaceEmptyStrWithNull({ a: '', b: 'b', c: ' ', d: 'd' }));
+console.log(replaceEmptyStrWithNull({ a: 'a', b: 'b ', c: ' ', d: '' }));
